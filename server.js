6877acef7faf40c7fb032dd0a5668b62e1c52000
Misa <91500465+MisaelC02@users.noreply.github.com>
2022-04-15 16:40:10 -0300

    baseactualizada

--- server.js
+++ server.js
@@ -3,13 +3,40 @@ const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mysql = require("mysql");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const sessions = sessions("sessions");
 const port = 2500;
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "",
+});
+
+db.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("concetado al servidor con el id" + db.threadId);
+});
 
 app.get("/", (req, res) => {
     res.render("File");
